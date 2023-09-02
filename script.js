c11dd4baa2c2d93bc2e88418435509c8503d8348
Othavio Luis <76593685+OthavioLuis@users.noreply.github.com>
2023-09-02 15:01:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function myBack() {
+    var botao = document.getElementById("teste_back");
+    var cor = document.getElementById("teste_back")
+ botao.style.background = "black";
+ cor.style.fontSize = "20px";
+ cor.style.color = "white";
+}
+
+
+function myFunction() {
+    var botao = document.getElementById("teste_back");
+    var cor = document.getElementById("teste_back");
+botao.style.background = "white";
+cor.style.fontSize = "20px";
+cor.style.color = "black";
+}
