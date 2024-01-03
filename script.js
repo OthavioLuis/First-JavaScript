ad9c6749cf49f244b7a754eb5fb7c92f7a13e932
OthavioLuis] <othaviorodriguesf@gmail.com>
2024-01-03 20:27:17 -0300

    aprimoramento do código js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
+var botao = document.getElementById("teste_back");
+var cor = document.getElementById("teste_back");
+
 function myBack() {
-    var botao = document.getElementById("teste_back");
-    var cor = document.getElementById("teste_back")
- botao.style.background = "black";
- cor.style.fontSize = "20px";
- cor.style.color = "white";
+    botao.style.background = "black";
+    cor.style.fontSize = "20px";
+    cor.style.color = "white";
 }
 
 
 function myFunction() {
-    var botao = document.getElementById("teste_back");
-    var cor = document.getElementById("teste_back");
-botao.style.background = "white";
-cor.style.fontSize = "20px";
-cor.style.color = "black";
+    botao.style.background = "white";
+    cor.style.fontSize = "20px";
+    cor.style.color = "black";
 }
