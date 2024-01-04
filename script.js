745f51909dcffd6aa67b396dd3710ce1076729e8
OthavioLuis] <othaviorodriguesf@gmail.com>
2024-01-03 21:46:43 -0300

    Exclusão de linhas inútes

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ var cor = document.getElementById("teste_back");
 
 function myBack() {
     botao.style.background = "black";
-    cor.style.fontSize = "20px";
     cor.style.color = "white";
 }
 
 
 function myFunction() {
     botao.style.background = "white";
-    cor.style.fontSize = "20px";
     cor.style.color = "black";
 }
